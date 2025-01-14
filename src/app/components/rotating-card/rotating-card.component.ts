7b5edc8f651dc48dd35a17fd274512490acfc469
aymensellaouti <aymen.sellaouti@gmail.com>
2025-01-14 16:15:59 +0100

    exo rotating card

--- src/app/components/rotating-card/rotating-card.component.ts
+++ src/app/components/rotating-card/rotating-card.component.ts
@@ -1,12 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-rotating-card',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './rotating-card.component.html',
-  styleUrl: './rotating-card.component.css'
+  styleUrl: './rotating-card.component.css',
 })
 export class RotatingCardComponent {
-
+  firstname = signal('aymen');
+  name = signal('sellaouti');
+  job = signal('Consultant');
+  path = signal('rotating_card_profile.png');
 }
